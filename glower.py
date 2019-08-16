64fea3d4c06990cdf4048d1663bea23ee3f47fdd
cududont <45621973+cududont@users.noreply.github.com>
2019-08-16 16:16:45 +0300

    Add files via upload

--- glower.py
+++ glower.py
@@ -7,18 +7,38 @@ dwGlowObjectManager = (0x52470F0)
 dwEntityList = (0x4D06CB4)
 m_iTeamNum = (0xF4)
 m_iGlowIndex = (0xA40C)
+dwLocalPlayer = (0xCF4A3C)
 
 try: #hook to csgo
     pym = pymem.Pymem("csgo.exe")
     client = pymem.process.module_from_name(pym.process_handle, "client_panorama.dll").lpBaseOfDll
 except:
     print("CSGO was not detected, closing application.")
-    sleep(5)
+    sleep(3)
     exit()
 
+#color vars 
+tcolorr = None 
+tcolorb = None
+ctcolorr = None 
+ctcolorb = None
 
 while True:
     glow = pym.read_int(dwGlowObjectManager + client)
+    lp = pym.read_int(dwLocalPlayer + client)
+    lpt = pym.read_int(lp + m_iTeamNum) #local player's team
+
+    if lpt == 2: #t
+        ctcolorr = 1
+        ctcolorb = 0
+        tcolorr = 0
+        tcolorb = 1
+
+    elif lpt == 3: #ct
+        tcolorr = 1
+        tcolorb = 0 
+        ctcolorr = 0
+        ctcolorb = 1
 
     for i in range(1, 20): #should be entities 1-10 but chickens fuck shit up   
         player = pym.read_int(client + dwEntityList + i * 0x10)
@@ -28,13 +48,15 @@ while True:
             playerglow = pym.read_int(player + m_iGlowIndex)
 
             if team == 2: #t
-                pym.write_float(glow + playerglow * 0x38 + 0x4, float(0.7)) #Red 
-                pym.write_float(glow + playerglow * 0x38 + 0x10, float(1)) #Opacity
-                pym.write_int(glow + playerglow * 0x38 + 0x24, int(1)) #Enable Glow
+                pym.write_float(glow + playerglow * 0x38 + 0x4, float(tcolorr)) #Red
+                pym.write_float(glow + playerglow * 0x38 + 0xC, float(tcolorb)) #Blue
+                pym.write_float(glow + playerglow * 0x38 + 0x10, float(0.8)) #Opacity
+                pym.write_int(glow + playerglow * 0x38 + 0x24, int(1)) 
                 
             elif team == 3: #ct
-                pym.write_float(glow + playerglow * 0x38 + 0xC, float(0.7)) #Blue
-                pym.write_float(glow + playerglow * 0x38 + 0x10, float(1)) #Opacity
-                pym.write_int(glow + playerglow * 0x38 + 0x24, int(1)) #Enable Glow 
+                pym.write_float(glow + playerglow * 0x38 + 0x4, float(ctcolorr)) #Red
+                pym.write_float(glow + playerglow * 0x38 + 0xC, float(ctcolorb)) #Blue
+                pym.write_float(glow + playerglow * 0x38 + 0x10, float(0.8)) #Opacity
+                pym.write_int(glow + playerglow * 0x38 + 0x24, int(1))  
 
 
